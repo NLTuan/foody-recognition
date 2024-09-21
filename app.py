6fe60aa2bd980ba716b622195014a76b8c7bf537
Tony <lthnguyen27@gmail.com>
2024-09-20 21:22:54 -0400

    finishing touches

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
+import PIL.Image
 import gradio as gr
 import torch
 from torch import nn
 import timm
-import PIL
+from PIL import Image
 from classes import classes
+import torchvision.transforms.functional as TF
+from pathlib import Path
+import os 
 
 device = 'cpu'
 def get_model():
     model = timm.create_model('levit_128s.fb_dist_in1k', pretrained=True)
     model.eval()
     model.fc = nn.Linear(2048, 101)
+    model.head.linear = nn.Linear(384, 101)
+    model.head_dist.linear = nn.Linear(384, 101)
     return model.to(device)
 
-model = get_model()
-data_config = timm.data.resolve_model_data_config(model)
-tfs = timm.data.create_transform(**data_config, is_training=False)
-model.load_state_dict(torch.load('model.pt', map_location=torch.device(device)))
-
 def image_classifier(inp):
-    inp = tfs(torch.tensor(inp).permute(2,0,1).unsqueeze(0).float())
+    img = Image.fromarray(inp)
+    inp = tfs(img).unsqueeze(0)
     out = model(inp)
     preds = out.softmax(1)
     top_5 = torch.argsort(preds, dim=1)[:5]
-    import pdb; pdb.set_trace()
-    return {classes[i.item()]: preds[i.item()] for i in top_5}
+    return {classes[i.item()]: preds[0][i.item()] for i in top_5[0]}
 
-demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label")
+model = get_model()
+model.load_state_dict(torch.load('model.pt', map_location=torch.device(device), weights_only=True))
+data_config = timm.data.resolve_model_data_config(model)
+tfs = timm.data.create_transform(**data_config, is_training=False)
+
+nms = os.listdir('./examples')
+examples = []
+for nm in nms:
+    examples.append(Image.open(Path('examples') / nm))
+
+demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label", examples=examples)
 demo.launch()
